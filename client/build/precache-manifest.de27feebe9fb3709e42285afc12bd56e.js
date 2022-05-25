21732ea8aea6f3064f043078b0bb3b106f46c23c
Harsh Rathod <hrathore50@ymail.com>
2022-05-25 15:44:46 +0530

    Built.

---
+++ client/build/precache-manifest.de27feebe9fb3709e42285afc12bd56e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d9501f7ea7738441d8b2e2042422c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20591d24505ec3d27b80",
+    "url": "/static/css/main.ebc8f33d.chunk.css"
+  },
+  {
+    "revision": "6a1ed8f1ab78e11b7f75",
+    "url": "/static/js/2.3aa47302.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.3aa47302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20591d24505ec3d27b80",
+    "url": "/static/js/main.9660ab8e.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "6edcbe7e99a086556eace77ab8158a8f",
+    "url": "/static/media/gigatorb-logo.6edcbe7e.png"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
